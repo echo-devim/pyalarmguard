269e420ecaaa9e36a233949ed905adf5c0ab3aaf
echo-devim <devim@outlook.it>
2023-06-05 21:16:58 +0100

    Removed config.py tracking

--- config.py
+++
@@ -1,7 +0,0 @@
-
-# App configuration file
-
-db_threshold = -73 # dB level threshold, lower values increase sensitivity
-high_pass_filter_cutoff = "4k"
-alarm_detection = True # Enable/Disable audio analysis for alarm detection
-recording_seconds = 2 # Number of seconds to record for alarm detection
